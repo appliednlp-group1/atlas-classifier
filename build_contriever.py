b31aa6e654ad7096c30198c971767bfd61de3eb1
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2022-12-20 00:23:36 +0900

    update build_contriever

--- build_contriever.py
+++ build_contriever.py
@@ -1,5 +1,7 @@
 import torch
 import transformers
+from transformers.models.rag.retrieval_rag import CustomHFIndex
+from datasets import load_from_disk
 
 
 def build_q_tokenizer(contriever_model: str) -> transformers.AutoTokenizer:
@@ -18,6 +20,15 @@ def build_config(bert_model: str,
                                   generator=classifier.config.to_dict())
 
 
+def _build_index(hidden_dim: int,
+                 dataset_path: str,
+                 ) -> CustomHFIndex:
+    dataset = load_from_disk(dataset_path)
+    return CustomHFIndex(hidden_dim,
+                         dataset,
+                         )
+
+
 def build_q_encoder(bert_model: str,
                     contriever_path: str) -> transformers.BertModel:
     checkpoint = torch.load(contriever_path, map_location='cpu')
@@ -33,3 +44,53 @@ def build_q_encoder(bert_model: str,
         if k.startswith('retriever.module.contriever.encoder.')
         })
     return q_encoder
+
+
+def build_retriever(bert_model: str,
+                    contriever_model: str,
+                    dataset_path: str,
+                    hidden_dim: int = 786,
+                    ) -> transformers.RagRetriever:
+    config = build_config(bert_model,
+                          contriever_model)
+    q_tokenizer = build_q_tokenizer(contriever_model)
+    g_tokenizer = _build_g_tokenizer(bert_model)
+    index = _build_index(hidden_dim,
+                         dataset_path)
+    return transformers.RagRetriever(config,
+                                     q_tokenizer,
+                                     g_tokenizer,
+                                     index=index)
+
+
+if __name__ == '__main__':
+    import torch
+    
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    
+    opt = {
+        'bert_model': 'bert-base-uncased',
+        'contriever_model': 'facebook/contriever',
+        'contriever_path': '../models/models/atlas/base/model.pth.tar',
+        'dataset_path': '../data/datasets/',
+    }
+    tokenizer = build_q_tokenizer(opt['contriever_model'])
+    retriever = build_retriever(opt['bert_model'],
+                                opt['contriever_model'],
+                                opt['dataset_path'])
+    q_encoder = build_q_encoder(opt['bert_model'],
+                                opt['contriever_path'])
+    
+    inputs = tokenizer('2005年から2015年で埼玉県の人口は変わっていますか？')
+    enc_outputs = q_encoder(torch.Tensor([inputs['input_ids']]).long().to(device),
+                            attention_mask=torch.Tensor([inputs['attention_mask']]).to(device),
+                            return_dict=True)
+    out = retriever(torch.Tensor([inputs['input_ids']]).long(),
+                    enc_outputs[0].cpu().clone().detach().to(torch.float32).numpy(),
+                    n_docs=5,
+                    return_tensors='pt')
+    ans = tokenizer.batch_decode(out['context_input_ids'],
+                                 skip_special_tokens=True,
+                                 clean_up_tokenization_spaces=True)
+    for a in ans:
+        print(a)
