8e90b238f29b1636e52897552ccb9175c5ab091a
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2022-12-19 23:11:05 +0900

    apply for batched

--- test_index.py
+++ test_index.py
@@ -10,21 +10,22 @@ def run(bert_model: str,
         contriever_path: str) -> None:
     tokenizer = build_q_tokenizer(contriever_model)
     q_encoder = build_q_encoder(bert_model, contriever_path)
+    q_encoder.cuda()
     q_encoder.eval()
 
     with torch.no_grad():
         dataset = load_dataset('ag_news')
         
         def process(example):
-            inputs = tokenizer(example['text'])
+            inputs = tokenizer(example['text'], truncation=True)
             example['embeddings'] = q_encoder(
-                input_ids=torch.tensor([inputs['input_ids']]).long(),
-                attention_mask=torch.tensor([inputs['attention_mask']]),
+                input_ids=torch.tensor(inputs['input_ids']).long().cuda(),
+                attention_mask=torch.tensor(inputs['attention_mask']).cuda(),
                 return_dict=True,
                 )[0].cpu().detach().numpy().tolist()
             return example
         
-        dataset = dataset.map(process)
+        dataset = dataset.map(process, batched=True)
     
     index = CustomHFIndex(768,
                           dataset)
