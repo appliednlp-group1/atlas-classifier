4955ed167b40f5c4144f513e7bb09cbd464c11d6
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2022-12-19 23:25:52 +0900

    update to save dataset

--- save_dataset.py
+++ save_dataset.py
@@ -1,13 +1,15 @@
+import os.path
+
 import torch
 from datasets import load_dataset
-from transformers.models.rag.retrieval_rag import CustomHFIndex
 
 from build_contriever import build_q_encoder, build_q_tokenizer
 
 
 def run(bert_model: str,
         contriever_model: str,
-        contriever_path: str) -> None:
+        contriever_path: str,
+        index_dir: str) -> None:
     tokenizer = build_q_tokenizer(contriever_model)
     q_encoder = build_q_encoder(bert_model, contriever_path)
     q_encoder.cuda()
@@ -26,17 +28,17 @@ def run(bert_model: str,
                 input_ids=torch.tensor(inputs['input_ids']).long().cuda(),
                 attention_mask=torch.tensor(inputs['attention_mask']).cuda(),
                 return_dict=True,
-                )[0].cpu().detach().numpy().tolist()
+                )[0].cpu().clone().detach().numpy().tolist()
             return example
         
         dataset = dataset.map(process, batched=True, batch_size=64)
     
-    index = CustomHFIndex(768,
-                          dataset)
-    print(index)
+    dataset.save_to_disk(os.path.join(index_dir, 'datasets'))
+    dataset.save_faiss_index('agnews', os.path.join(index_dir, 'agnews.faiss'))
 
 
 if __name__ == '__main__':
     run('bert-base-uncased',
         'facebook/contriever',
-        '../models/models/atlas/base/model.pth.tar')
+        '../models/models/atlas/base/model.pth.tar',
+        '../data')
