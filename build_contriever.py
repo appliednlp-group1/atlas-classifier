72e1c4889535c446d0b1081266217ab5ae766c50
nakai-yu <nakai-yu623@g.ecc.u-tokyo.ac.jp>
2022-12-20 00:37:17 +0900

    fix bugs

--- build_contriever.py
+++ build_contriever.py
@@ -23,7 +23,12 @@ def _build_index(hidden_dim: int,
                  dataset_path: str,
                  ) -> CustomHFIndex:
     dataset = load_from_disk(dataset_path)['train']
-    dataset['title'] = ''
+    
+    def add_title(example):
+        example['title'] = ''
+        return example
+    
+    dataset = dataset.map(add_title, batched=True)
     return CustomHFIndex(hidden_dim,
                          dataset,
                          )
