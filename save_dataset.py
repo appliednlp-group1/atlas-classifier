d7ea8bf2b578e8746fe15a7d198dcd0eec88c57f
nnaakkaaii <ultraganbayu10@gmail.com>
2022-12-28 22:00:04 +0900

    fix

--- save_dataset.py
+++ save_dataset.py
@@ -39,7 +39,7 @@ def run(bert_model: str,
     dataset.save_to_disk(os.path.join(index_dir, 'datasets'))
     
     ds = dataset['train']
-    ds = ds.add_faiss_index('embeddings')
+    ds = ds.add_faiss_index('agnews')
     
     ds.save_faiss_index('agnews', os.path.join(index_dir, 'agnews.faiss'))
 
